4fe82eb6d751dfa7cb23670173e89b44e8a4088b
aspandya369 <96020460+aspandya369@users.noreply.github.com>
2022-01-05 23:20:06 +0530

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,3 @@
+a = int(input("Enter Number (to add with 25) : ")) #input as int
+func = lambda X:X+25                               # lambda func
+print('The output after adding 25 is : ' , func(a))#Printing output
