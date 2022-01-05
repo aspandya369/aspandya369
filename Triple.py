655961edfddc57fbde411e4353805620065a9d26
aspandya369 <96020460+aspandya369@users.noreply.github.com>
2022-01-05 23:24:51 +0530

    Update Triple.py

--- Triple.py
+++ Triple.py
@@ -1,3 +1,3 @@
 num = (1, 2, 3, 4, 5, 6, 7) #input
-result = map(lambda x: x + x + x, num) #Doing triple
+result = map(lambda x: x * 3, num) #Doing triple
 print("Triple of said list numbers:",list(result))#output
